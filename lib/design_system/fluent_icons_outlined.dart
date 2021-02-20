75df32ad43ec3d48ac4c056c828aac009d56f1b0
Roshan Singh <iprogramyourapp@gmail.com>
2021-02-20 20:40:47 +0530

    Iconography DONE!!

---
+++ lib/design_system/fluent_icons_outlined.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/widgets.dart';
+
+class FluentIconsOutlined {
+  FluentIconsOutlined._();
+
+  static const _kFontFam = 'FluentIconsOutlined';
+
+  static const IconData ios_checkmark_outline =
+      IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData ios_chevron_down_outline =
+      IconData(0xe801, fontFamily: _kFontFam);
+  static const IconData ios_dismiss_outline =
+      IconData(0xe802, fontFamily: _kFontFam);
+  static const IconData ios_dismiss_circle_outline =
+      IconData(0xe803, fontFamily: _kFontFam);
+  static const IconData ios_grid_outline =
+      IconData(0xe804, fontFamily: _kFontFam);
+  static const IconData ios_left_arrow_outline =
+      IconData(0xe805, fontFamily: _kFontFam);
+  static const IconData ios_chevron_right_outline =
+      IconData(0xe806, fontFamily: _kFontFam);
+  static const IconData ios_more_outline =
+      IconData(0xe807, fontFamily: _kFontFam);
+  static const IconData ios_add_outline_1 =
+      IconData(0xe808, fontFamily: _kFontFam);
+  static const IconData ios_alert_outline =
+      IconData(0xe809, fontFamily: _kFontFam);
+  static const IconData ios_calendar_outline =
+      IconData(0xe80a, fontFamily: _kFontFam);
+  static const IconData ios_mail_outline =
+      IconData(0xe80b, fontFamily: _kFontFam);
+  static const IconData ios_phone_outline =
+      IconData(0xe80c, fontFamily: _kFontFam);
+  static const IconData ios_chat_outline =
+      IconData(0xe80d, fontFamily: _kFontFam);
+  static const IconData ios_mention_outline =
+      IconData(0xe80e, fontFamily: _kFontFam);
+  static const IconData ios_search_outline =
+      IconData(0xe80f, fontFamily: _kFontFam);
+  static const IconData android_add_outline =
+      IconData(0xe810, fontFamily: _kFontFam);
+  static const IconData android_arrow_left_outline =
+      IconData(0xe811, fontFamily: _kFontFam);
+  static const IconData android_checkmark_outline =
+      IconData(0xe812, fontFamily: _kFontFam);
+  static const IconData android_chevron_down_outline =
+      IconData(0xe813, fontFamily: _kFontFam);
+  static const IconData android_copy_outline =
+      IconData(0xe814, fontFamily: _kFontFam);
+  static const IconData android_delete_outline =
+      IconData(0xe815, fontFamily: _kFontFam);
+  static const IconData android_dismiss_outline =
+      IconData(0xe816, fontFamily: _kFontFam);
+  static const IconData android_dismiss_circle =
+      IconData(0xe817, fontFamily: _kFontFam);
+  static const IconData android_filter_outline =
+      IconData(0xe818, fontFamily: _kFontFam);
+  static const IconData android_flag_outline =
+      IconData(0xe819, fontFamily: _kFontFam);
+  static const IconData android_chevron_right_outline =
+      IconData(0xe81a, fontFamily: _kFontFam);
+  static const IconData android_link_outline =
+      IconData(0xe81b, fontFamily: _kFontFam);
+  static const IconData android_mail_outline =
+      IconData(0xe81c, fontFamily: _kFontFam);
+  static const IconData android_mic_on =
+      IconData(0xe81d, fontFamily: _kFontFam);
+  static const IconData android_more_vertical_outline =
+      IconData(0xe81e, fontFamily: _kFontFam);
+  static const IconData android_person_outline =
+      IconData(0xe81f, fontFamily: _kFontFam);
+  static const IconData android_phone_outline =
+      IconData(0xe820, fontFamily: _kFontFam);
+  static const IconData android_play_outline =
+      IconData(0xe821, fontFamily: _kFontFam);
+  static const IconData android_search_outline =
+      IconData(0xe822, fontFamily: _kFontFam);
+  static const IconData android_share_outline =
+      IconData(0xe823, fontFamily: _kFontFam);
+}
